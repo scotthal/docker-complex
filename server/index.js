7ac0e7d153732ad7a263b8e64260ecdc807d06b2
Scott Hallock <scott@hallocks.net>
2021-04-25 21:07:52 +0000

    Initial database connection and creation

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const keys = require("./keys");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+});
+
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.error(err));
+});
